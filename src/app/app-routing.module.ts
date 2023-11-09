b3b1f0f765e5fb136300435e8b5845d333536abc
Giselle Torres <giselita6313@gmail.com>
2023-11-08 19:06:19 -0500

    imlementar login 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-//import { MenuInicioComponent } from './menu-inicio/menu-inicio.component';
+import { MenuInicioComponent } from './menu-inicio/menu-inicio.component';
 //import { RegisterComponent } from './users/register/register.component';
-import { LoginComponent } from './users/login/login.component';
+//import { LoginComponent } from './users/login/login.component';
 const routes: Routes = [
  // {path: '', component: MenuInicioComponent},
  // {path: '', component: RegisterComponent},
-  {path: '', component: LoginComponent}
+  {path: '', component: MenuInicioComponent}
 ];
 
 @NgModule({
